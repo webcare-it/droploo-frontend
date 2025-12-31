d1fe583a34faa6b186857da47f9b0912b6e0d978
Webcare-it <ceowebcareit@gmail.com>
2025-12-31 18:56:18 +0600

    [FIX] navigation

--- src/components/layout/header/mobile.tsx
+++ src/components/layout/header/mobile.tsx
@@ -4,24 +4,68 @@ import { Link } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { navigationMenuItem } from "@/components/ui/navigation-menu";
 import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export const MobileHeader = () => {
   return (
     <div className="flex md:hidden px-4 items-center justify-between">
+      <MobileHead>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Menu
+              size={30}
+              strokeWidth={3}
+              absoluteStrokeWidth
+              className="text-primary"
+            />
+          </SheetTrigger>
+          <SheetContent className="h-full w-full">
+            <div className="grid flex-1 auto-rows-min gap-6 px-4">
+              <div className="grid gap-3">
+                <Label htmlFor="sheet-demo-name">Name</Label>
+                <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="sheet-demo-username">Username</Label>
+                <Input id="sheet-demo-username" defaultValue="@peduarte" />
+              </div>
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button
+                  variant="outline"
+                  className={cn(
+                    "text-destructive border-destructive hover:bg-destructive/5 hover:text-destructive"
+                  )}>
+                  Close
+                </Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      </MobileHead>
+    </div>
+  );
+};
+
+export const MobileHead = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
       <HeaderLogo />
       <div className="flex items-center gap-4 shrink-0">
         <Button variant="ghost" asChild className={cn(navigationMenuItem())}>
           <Link to="/signin">Sign in</Link>
         </Button>
-        <button>
-          <Menu
-            size={30}
-            strokeWidth={3}
-            absoluteStrokeWidth
-            className="text-primary"
-          />
-        </button>
+        {children}
       </div>
-    </div>
+    </>
   );
 };
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -1,29 +1,29 @@
-import * as React from "react"
-import * as SheetPrimitive from "@radix-ui/react-dialog"
-import { XIcon } from "lucide-react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { MobileHead } from "../layout/header/mobile";
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
-  return <SheetPrimitive.Root data-slot="sheet" {...props} />
+  return <SheetPrimitive.Root data-slot="sheet" {...props} />;
 }
 
 function SheetTrigger({
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Trigger>) {
-  return <SheetPrimitive.Trigger data-slot="sheet-trigger" {...props} />
+  return <SheetPrimitive.Trigger data-slot="sheet-trigger" {...props} />;
 }
 
 function SheetClose({
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Close>) {
-  return <SheetPrimitive.Close data-slot="sheet-close" {...props} />
+  return <SheetPrimitive.Close data-slot="sheet-close" {...props} />;
 }
 
 function SheetPortal({
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Portal>) {
-  return <SheetPrimitive.Portal data-slot="sheet-portal" {...props} />
+  return <SheetPrimitive.Portal data-slot="sheet-portal" {...props} />;
 }
 
 function SheetOverlay({
@@ -39,16 +39,16 @@ function SheetOverlay({
       )}
       {...props}
     />
-  )
+  );
 }
 
 function SheetContent({
   className,
   children,
-  side = "right",
+  side = "top",
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
-  side?: "top" | "right" | "bottom" | "left"
+  side?: "top" | "right" | "bottom" | "left";
 }) {
   return (
     <SheetPortal>
@@ -67,16 +67,24 @@ function SheetContent({
             "data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom-0 h-auto border-t",
           className
         )}
-        {...props}
-      >
+        {...props}>
+        <div className="flex items-center justify-between p-4">
+          <MobileHead>
+            <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-primary rounded-xs  transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
+              <X
+                size={32}
+                strokeWidth={3}
+                absoluteStrokeWidth
+                className="text-destructive"
+              />
+              <span className="sr-only">Close</span>
+            </SheetPrimitive.Close>
+          </MobileHead>
+        </div>
         {children}
-        <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          <XIcon className="size-4" />
-          <span className="sr-only">Close</span>
-        </SheetPrimitive.Close>
       </SheetPrimitive.Content>
     </SheetPortal>
-  )
+  );
 }
 
 function SheetHeader({ className, ...props }: React.ComponentProps<"div">) {
@@ -86,7 +94,7 @@ function SheetHeader({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("flex flex-col gap-1.5 p-4", className)}
       {...props}
     />
-  )
+  );
 }
 
 function SheetFooter({ className, ...props }: React.ComponentProps<"div">) {
@@ -96,7 +104,7 @@ function SheetFooter({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("mt-auto flex flex-col gap-2 p-4", className)}
       {...props}
     />
-  )
+  );
 }
 
 function SheetTitle({
@@ -109,7 +117,7 @@ function SheetTitle({
       className={cn("text-foreground font-semibold", className)}
       {...props}
     />
-  )
+  );
 }
 
 function SheetDescription({
@@ -122,7 +130,7 @@ function SheetDescription({
       className={cn("text-muted-foreground text-sm", className)}
       {...props}
     />
-  )
+  );
 }
 
 export {
@@ -134,4 +142,4 @@ export {
   SheetFooter,
   SheetTitle,
   SheetDescription,
-}
+};
